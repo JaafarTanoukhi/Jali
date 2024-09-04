41303e201dcdaf65408a98b9d7ff7946423c6b7a
somerandim <rickandmorty2006@gmail.com>
2024-09-04 10:14:56 +0300

    frontend html authpage

---
+++ jali-client/src/app/Auth/signup-customer/signup-customer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-customer.component.html',
+  styleUrl: './signup-customer.component.css'
+})
+export class SignupCustomerComponent {
+
+}
--- jali-client/src/app/app.component.ts
+++ jali-client/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogInComponent } from "./Auth/log-in/log-in.component";
+import { SignupCustomerComponent } from "./Auth/signup-customer/signup-customer.component";
+import { SignupSellerComponent } from "./Auth/signup-seller/signup-seller.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogInComponent, SignupCustomerComponent, SignupSellerComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
